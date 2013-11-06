9cc35cdafc55d7c9873e4b406d07ca8e996b5bda
Pavel Lobodinský <plobodinsky@vendavo.com>
2013-11-06 22:33:19 +0100

    Fixed unit tests to work correctly if run under root user - root has rw permissions granted to all dirs/files and x permissions to those files which have x permission for at least one of the owner/group/others

--- test/test.js
+++ test/test.js
@@ -3,7 +3,7 @@
 // TODO test on debian linux
 /**
  * Unit tests check owner dir/file permissions only because it is not possible to run chmod when not being dir/file owner.
- * In order to test group/other permissions, this test would need to change owner to some other user, and would need to know root password to run 'sudo chmod'. This is desirable.
+ * In order to test group/other permissions, this test would need to change owner to some other user, and would need to know rootUser password to run 'sudo chmod'. This is desirable.
  *
  * Recalling UNIX permissions:
  *   R = dec 4 (bin 100)
@@ -22,25 +22,31 @@ var testFile = __dirname + '/test-file.txt';
 // original permissions so that we can restore them once all unit tests are finalised
 var testDirMode;
 var testFileMode;
+// true if the test is run by root user
+var rootUser = false;
 
 describe(pkg.name, function() {
     before(function() {
         // id of user this process is running under
         var uid = process.getuid();
+        // are we running under rootUser? if so, tests must take this into account because rootUser has access everywhere even when the permissions are 000
+        rootUser = uid === 0;
 
         // verify that UID of the testing dir/file are the same as of this process because only dir/file owner can call chmod command
         // chmod command is essential to allow unit tests to change dir/file permissions and verify results of the access() calls
         var stats = fs.statSync(testDir);
         should.exist(stats);
         stats.isDirectory().should.equal(true);
-        stats.uid.should.equal(uid, 'UID of the node.js process must be the owner of \'' + testDir + '\' directory to allow permission manipulation via chmod command.');
+        if (!rootUser)
+            uid.should.equal(stats.uid, 'You run the tests under a different user than the one, who has installed this module. As a consequence, UID of the node.js process (UID=' + uid + ') differs from the owner of \'' + testDir + '\' (UID=' + stats.uid + ') which prevents permission manipulation via chmod command and thus prevents the unit tests to work.');
         // save original permissions to get restored later
         testDirMode = stats.mode;
 
         stats = fs.statSync(testFile);
         should.exist(stats);
         stats.isFile().should.equal(true);
-        stats.uid.should.equal(uid, 'UID of the node.js process must be the owner of  \'' + testFile + '\' file to allow permission manipulation via chmod command.');
+        if (!rootUser)
+            uid.should.equal(stats.uid, 'You run the tests under a different user than the one, who has installed this module. As a consequence, UID of the node.js process (UID=' + uid + ') differs from the owner of \'' + testFile + '\' (UID=' + stats.uid + ') which prevents permission manipulation via chmod command and thus prevents the unit tests to work.');
         // save original permissions to get restored later
         testFileMode = stats.mode;
     });
@@ -126,28 +132,29 @@ describe(pkg.name, function() {
             fs.chmodSync(testDir, '000');
             fs.chmodSync(testFile, '000');
 
-            // tests include case-insensitivity of permission characters as well as ignoring invalid characters
-            access.sync(testDir, 'r').should.equal(false);
-            access.sync(testDir, 'w').should.equal(false);
-            access.sync(testDir, 'x').should.equal(false);
-            access.sync(testDir, 'rw').should.equal(false);
-            access.sync(testDir, 'rx').should.equal(false);
-            access.sync(testDir, 'wx').should.equal(false);
-            access.sync(testDir, 'rwx').should.equal(false);
+            // if the test is run by a root user, we must expect true values where there are false values for other users (root user has access everywhere even when permissions are 000)
+            access.sync(testDir, 'r').should.equal(rootUser);
+            access.sync(testDir, 'w').should.equal(rootUser);
+            access.sync(testDir, 'x').should.equal(rootUser);
+            access.sync(testDir, 'rw').should.equal(rootUser);
+            access.sync(testDir, 'rx').should.equal(rootUser);
+            access.sync(testDir, 'wx').should.equal(rootUser);
+            access.sync(testDir, 'rwx').should.equal(rootUser);
 
             access.async(testFile, 'r', function(err, result) {
                 should.not.exist(err);
                 should.exist(result);
-                result.should.equal(false);
+                result.should.equal(rootUser);
 
                 access.async(testFile, 'w', function(err, result) {
                     should.not.exist(err);
                     should.exist(result);
-                    result.should.equal(false);
+                    result.should.equal(rootUser);
 
                     access.async(testFile, 'x', function(err, result) {
                         should.not.exist(err);
                         should.exist(result);
+                        // will be false even for root user - executable permission on a file is is granted to super user only, if at least one of owner/group/others has executable permission
                         result.should.equal(false);
 
                         done();
@@ -161,11 +168,11 @@ describe(pkg.name, function() {
             fs.chmodSync(testFile, '501'); // RX permissions for the owner
 
             // tests include case-insensitivity of permission characters as well as ignoring invalid characters
-
+            // if the test is run by a root user, we must expect true values where there are false values for other users (root user has access everywhere even when permissions are 000)
             access.sync(testDir, 'blah W   x oops').should.equal(true);
-            access.sync(testDir, ' R w X blah ').should.equal(false);
+            access.sync(testDir, ' R w X blah ').should.equal(rootUser);
             access.sync(testFile, ' R oops x').should.equal(true);
-            access.sync(testFile, 'wr oops X').should.equal(false);
+            access.sync(testFile, 'wr oops X').should.equal(rootUser);
 
             access.async(testDir, 'x W kooky', function(err, result) {
                 should.not.exist(err);
@@ -185,7 +192,7 @@ describe(pkg.name, function() {
                         access.async(testFile, 'does not have write permissions ', function(err, result) {
                             should.not.exist(err);
                             should.exist(result);
-                            result.should.equal(false);
+                            result.should.equal(rootUser);
 
                             done();
                         });
@@ -195,15 +202,16 @@ describe(pkg.name, function() {
         });
 
         it('should have R permissions only', function(done) {
-            fs.chmodSync(testDir, '400');  // R permissions for the owner
-            fs.chmodSync(testFile, '416'); // R permissions for the owner
+            fs.chmodSync(testDir, '410');  // R permissions for the owner
+            fs.chmodSync(testFile, '406'); // R permissions for the owner
 
+            // if the test is run by a root user, we must expect true values where there are false values for other users (root user has access everywhere even when permissions are 000)
             access.sync(testDir, 'r').should.equal(true);
-            access.sync(testDir, 'rw').should.equal(false);
-            access.sync(testDir, 'rx').should.equal(false);
-            access.sync(testDir, 'rwx').should.equal(false);
-            access.sync(testDir, 'w').should.equal(false);
-            access.sync(testDir, 'x').should.equal(false);
+            access.sync(testDir, 'rw').should.equal(rootUser);
+            access.sync(testDir, 'rx').should.equal(rootUser);
+            access.sync(testDir, 'rwx').should.equal(rootUser);
+            access.sync(testDir, 'w').should.equal(rootUser);
+            access.sync(testDir, 'x').should.equal(rootUser);
 
             access.async(testFile, 'r', function(err, result) {
                 should.not.exist(err);
@@ -213,26 +221,28 @@ describe(pkg.name, function() {
                 access.async(testFile, 'rx', function(err, result) {
                     should.not.exist(err);
                     should.exist(result);
+                    // will be false even for root user - executable permission on a file is is granted to super user only, if at least one of owner/group/others has executable permission
                     result.should.equal(false);
 
                     access.async(testFile, 'rw', function(err, result) {
                         should.not.exist(err);
                         should.exist(result);
-                        result.should.equal(false);
+                        result.should.equal(rootUser);
 
                         access.async(testFile, 'rws', function(err, result) {
                             should.not.exist(err);
                             should.exist(result);
-                            result.should.equal(false);
+                            result.should.equal(rootUser);
 
                             access.async(testFile, 'w', function(err, result) {
                                 should.not.exist(err);
                                 should.exist(result);
-                                result.should.equal(false);
+                                result.should.equal(rootUser);
 
                                 access.async(testFile, 'x', function(err, result) {
                                     should.not.exist(err);
                                     should.exist(result);
+                                    // will be false even for root user - executable permission on a file is is granted to super user only, if at least one of owner/group/others has executable permission
                                     result.should.equal(false);
 
                                     done();
@@ -248,13 +258,14 @@ describe(pkg.name, function() {
             fs.chmodSync(testDir, '340');  // WX permissions for the owner
             fs.chmodSync(testFile, '361'); // WX permissions for the owner
 
+            // if the test is run by a root user, we must expect true values where there are false values for other users (root user has access everywhere even when permissions are 000)
             access.sync(testFile, 'w').should.equal(true);
             access.sync(testFile, 'x').should.equal(true);
             access.sync(testFile, 'wx').should.equal(true);
-            access.sync(testFile, 'r').should.equal(false);
-            access.sync(testFile, 'rw').should.equal(false);
-            access.sync(testFile, 'rx').should.equal(false);
-            access.sync(testFile, 'rwx').should.equal(false);
+            access.sync(testFile, 'r').should.equal(rootUser);
+            access.sync(testFile, 'rw').should.equal(rootUser);
+            access.sync(testFile, 'rx').should.equal(rootUser);
+            access.sync(testFile, 'rwx').should.equal(rootUser);
 
             access.async(testDir, 'w', function(err, result) {
                 should.not.exist(err);
@@ -274,22 +285,22 @@ describe(pkg.name, function() {
                         access.async(testDir, 'r', function(err, result) {
                             should.not.exist(err);
                             should.exist(result);
-                            result.should.equal(false);
+                            result.should.equal(rootUser);
 
                             access.async(testDir, 'rw', function(err, result) {
                                 should.not.exist(err);
                                 should.exist(result);
-                                result.should.equal(false);
+                                result.should.equal(rootUser);
 
                                 access.async(testDir, 'rx', function(err, result) {
                                     should.not.exist(err);
                                     should.exist(result);
-                                    result.should.equal(false);
+                                    result.should.equal(rootUser);
 
                                     access.async(testDir, 'rwx', function(err, result) {
                                         should.not.exist(err);
                                         should.exist(result);
-                                        result.should.equal(false);
+                                        result.should.equal(rootUser);
 
                                         done();
                                     });
